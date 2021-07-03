c196f082626ff83ec614c0d3234c73181bc41a27
Rajib <48186532+rajibxiii@users.noreply.github.com>
2021-07-03 22:13:35 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import*
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+class faceRecognitionSystem:
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry("1200x790+0+0")
+        self.root.title("Face Recognition Student Attendance System")
+
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    obj = faceRecognitionSystem(root)
+    root.mainloop() 
