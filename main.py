7d91bf91d478aeeb970a4b274c7b4031f3d2811c
Sabit-Bin-Hamid <74405314+Sabit-Bin-Hamid@users.noreply.github.com>
2021-07-09 17:16:30 +0600

    done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+# from tkinter import ttk
 from PIL import Image, ImageTk
 
 
@@ -7,7 +7,7 @@ class FaceRecSys:
 
     def __init__(self, root):
         self.root = root
-        self.root.geometry('1500x790+0+0')
+        self.root.geometry('1530x790+0+0')
         self.root.title('Face Recognition Student Attendance System')
 
         # 1st Image
@@ -31,11 +31,11 @@ class FaceRecSys:
         # 3rd Image
         img3 = Image.open(
             r"images\color1.jpg")
-        img3 = img3.resize((500, 130), Image.ANTIALIAS)
+        img3 = img3.resize((550, 130), Image.ANTIALIAS)
         self.PhoImg3 = ImageTk.PhotoImage(img3)
 
         FirstLabel = Label(self.root, image=self.PhoImg3)
-        FirstLabel.place(x=1000, y=0, width=500, height=130)
+        FirstLabel.place(x=1000, y=0, width=530, height=130)
 
         # Background Image
         imgBg = Image.open(
@@ -52,8 +52,42 @@ class FaceRecSys:
 
         TitleLable.place(x=0, y=0, width=1530, height=50)
 
+        """
+          making  button for  'Student Details',' Face Detector' ,
+         'Train Data',' Attendance' , 'Photos', 'Exit' 
+        """
+        #student button(img4)
+        StudentBtn = Image.open(r"images\StudentButton.jpg")
+        StudentBtn = StudentBtn.resize((130,130),Image.ANTIALIAS)
+        self.PhoImgStdBtn = ImageTk.PhotoImage(StudentBtn)
+
+        Btn1 = Button(BgImg , image=self.PhoImgStdBtn, cursor='hand2')
+        Btn1.place(x=200 , y=100 , width=130,height=130)
+
+        Btn1 = Button(BgImg, text='Student detail', cursor='hand2',
+                      font=("times new Roman", 12, 'bold',),
+                      bg='darkblue', fg='white')
+        Btn1.place(x=200, y=200, width=129, height=30)
+
+        # Face Detect Button (img5)
+
+        FacDetactBtn = Image.open(r"images\facedetact.PNG")
+        FacDetactBtn = FacDetactBtn.resize((130, 130), Image.ANTIALIAS)
+        self.PhoImgFacDetBtn = ImageTk.PhotoImage(FacDetactBtn)
+
+        Btn2 = Button(BgImg, image=self.PhoImgFacDetBtn, cursor='hand2')
+        Btn2.place(x=450, y=100, width=130, height=130)
+
+        Btn2 = Button(BgImg, text='Face detact', cursor='hand2',
+                      font=("times new Roman", 12, 'bold',),
+                      bg='darkblue', fg='white')
+        Btn2.place(x=450, y=200, width=129, height=30)
+
+
+
+
 
 if __name__ == "__main__":
-    root = Tk()
+    root = Tk()#root is needed to call by toolkit (tk)
     obj = FaceRecSys(root)
     root.mainloop()
