5854f8d20d98f72b05fe7973d645f06699f58723
rajibxiii <rajibxiii@gmail.com>
2021-07-13 01:12:37 +0600

    Solved all the spelling mistakes. Updated to modern fonts. Code Formatted.

--- main.py
+++ main.py
@@ -19,48 +19,43 @@ class FaceRecSys:
         BgImg = Label(self.root, image=self.PhoImgBg)
         BgImg.place(x=0, y=0, width=1530, height=790)
 
-        TitleLable = Label(text="Student Attendance System",
-                        font=("Calibri Light", 30,),
-                        fg='black')
+        titleLabel = Label(text="Student Attendance System",
+                           font=("Calibri Light", 30,),
+                           fg='black')
 
-        TitleLable.place(x=0, y=120, width=1530, height=50)
+        titleLabel.place(x=0, y=120, width=1530, height=50)
 
-        """
-          making  button for  'Student Details',' Face Detector' ,
-         'Train Data',' Attendance' , 'Photos', 'Exit' 
-        """
-        #student button(img4)
-        StudentBtn = Image.open(r"images\StudentButton.jpg")
-        StudentBtn = StudentBtn.resize((130,130),Image.ANTIALIAS)
-        self.PhoImgStdBtn = ImageTk.PhotoImage(StudentBtn)
 
-        Btn1 = Button(BgImg , image=self.PhoImgStdBtn, cursor='hand2')
-        Btn1.place(x=200 , y=220 , width=130,height=130)
+        # Making  buttons for 'Student Details', 'Face Detector', 'Train Data', 'Attendance', 'Photos', 'Exit'
 
-        Btn1 = Button(BgImg, text='Student detail', cursor='hand2',
-                      font=("times new Roman", 12, 'bold',),
+        # Student button (img4)
+        studenButton = Image.open(r"images\StudentButton.jpg")
+        studenButton = studenButton.resize((130, 130), Image.ANTIALIAS)
+        self.PhoImgStdBtn = ImageTk.PhotoImage(studenButton)
+
+        Btn1 = Button(BgImg, image=self.PhoImgStdBtn, cursor='hand2')
+        Btn1.place(x=200, y=220, width=130, height=130)
+
+        Btn1 = Button(BgImg, text='STUDENT DETAILS', cursor='hand2',
+                      font=("Calibri", 12, 'bold',),
                       bg='darkblue', fg='white')
         Btn1.place(x=200, y=320, width=129, height=30)
 
         # Face Detect Button (img5)
-
-        FacDetactBtn = Image.open(r"images\facedetact.PNG")
-        FacDetactBtn = FacDetactBtn.resize((130, 130), Image.ANTIALIAS)
-        self.PhoImgFacDetBtn = ImageTk.PhotoImage(FacDetactBtn)
+        faceDetectButton = Image.open(r"images\facedetect.PNG")
+        faceDetectButton = faceDetectButton.resize((130, 130), Image.ANTIALIAS)
+        self.PhoImgFacDetBtn = ImageTk.PhotoImage(faceDetectButton)
 
         Btn2 = Button(BgImg, image=self.PhoImgFacDetBtn, cursor='hand2')
         Btn2.place(x=450, y=220, width=130, height=130)
 
-        Btn2 = Button(BgImg, text='Face detact', cursor='hand2',
-                      font=("times new Roman", 12, 'bold',),
+        Btn2 = Button(BgImg, text='DETECT FACE', cursor='hand2',
+                      font=("Calibri", 12, 'bold',),
                       bg='darkblue', fg='white')
         Btn2.place(x=450, y=320, width=129, height=30)
 
 
-
-
-
 if __name__ == "__main__":
-    root = Tk()#root is needed to call by toolkit (tk)
+    root = Tk()  # root is needed to call by toolkit (tk)
     obj = FaceRecSys(root)
     root.mainloop()
