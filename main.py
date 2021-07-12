8e37343c39c487d8504f01db5fdbd249be33cff4
Sabit-Bin-Hamid <74405314+Sabit-Bin-Hamid@users.noreply.github.com>
2021-07-13 04:04:37 +0600

    Update main.py
    ADDED Help desk and Attendance button

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class FaceRecSys:
 
         # Making  buttons for 'Student Details', 'Face Detector', 'Train Data', 'Attendance', 'Photos', 'Exit'
 
-        # Student button (img4)
+        # Student detail button (img4)
         studenButton = Image.open(r"images\StudentButton.jpg")
         studenButton = studenButton.resize((130, 130), Image.ANTIALIAS)
         self.PhoImgStdBtn = ImageTk.PhotoImage(studenButton)
@@ -37,13 +37,50 @@ class FaceRecSys:
         Btn1 = Button(BgImg, image=self.PhoImgStdBtn, cursor='hand2')
         Btn1.place(x=200, y=220, width=130, height=130)
 
-        Btn1 = Button(BgImg, text='STUDENT DETAILS', cursor='hand2',
+        Btn1 = Button(BgImg, text='Student detail', cursor='hand2',
                       font=("Calibri", 12, 'bold',),
-                      bg='black', fg='white')
+                      bg='darkblue', fg='white')
         Btn1.place(x=200, y=320, width=129, height=30)
 
+        # Face Detect Button (img5)
+        faceDetectButton = Image.open(r"images\facedetect.PNG")
+        faceDetectButton = faceDetectButton.resize((130, 130), Image.ANTIALIAS)
+        self.PhoImgFacDetBtn = ImageTk.PhotoImage(faceDetectButton)
 
+        Btn2 = Button(BgImg, image=self.PhoImgFacDetBtn, cursor='hand2')
+        Btn2.place(x=450, y=220, width=130, height=130)
 
+        Btn2 = Button(BgImg, text='Face detect', cursor='hand2',
+                      font=("Calibri", 12, 'bold',),
+                      bg='darkblue', fg='white')
+        Btn2.place(x=450, y=320, width=129, height=30)
+
+        # Attendance Button (img5)
+        AttendanceButton = Image.open(r"images\attendance1.PNG")
+        AttendanceButton = AttendanceButton.resize((130, 130), Image.ANTIALIAS)
+        self.PhoImgAttendBtn = ImageTk.PhotoImage(AttendanceButton)
+
+        Btn3 = Button(BgImg, image=self.PhoImgAttendBtn, cursor='hand2')
+        Btn3.place(x=700, y=220, width=130, height=130)
+
+        Btn3 = Button(BgImg, text='Attendance', cursor='hand2',
+                      font=("Calibri", 12, 'bold',),
+                      bg='darkblue', fg='white')
+        Btn3.place(x=700, y=320, width=129, height=30)
+
+        # Help desk button (image 6)
+
+        HelpDskButton = Image.open(r"images\helpdesk.PNG")
+        HelpDskButton = HelpDskButton.resize((130, 130), Image.ANTIALIAS)
+        self.PhoImgHelpdskBtn = ImageTk.PhotoImage(HelpDskButton)
+
+        Btn4 = Button(BgImg, image=self.PhoImgHelpdskBtn, cursor='hand2')
+        Btn4.place(x=950, y=220, width=130, height=130)
+
+        Btn4 = Button(BgImg, text='Help desk', cursor='hand2',
+                      font=("Calibri", 12, 'bold',),
+                      bg='darkblue', fg='white')
+        Btn4.place(x=950, y=320, width=129, height=30)
 
 
 if __name__ == "__main__":
