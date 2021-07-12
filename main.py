8619be5263ce5dd76d741d839490c2a28d085b46
rajibxiii <rajibxiii@gmail.com>
2021-07-13 02:05:46 +0600

    Finishing touch with colors

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ class FaceRecSys:
         self.root = root
         self.root.geometry('1530x790+0+0')
         self.root.title('Face Recognition Student Attendance System')
-        root.resizable(0,0)
-
-
+        root.resizable(0, 0)
+        root.attributes('-alpha', 0.95)
 
         # Background Image
         imgBg = Image.open(
@@ -24,10 +23,9 @@ class FaceRecSys:
 
         titleLabel = Label(text="Student Attendance System",
                            font=("Calibri Light", 30,),
-                           fg='black')
+                           bg='#E8F0F2', fg='black')
 
         titleLabel.place(x=0, y=120, width=1530, height=50)
-
 
         # Making  buttons for 'Student Details', 'Face Detector', 'Train Data', 'Attendance', 'Photos', 'Exit'
 
@@ -41,7 +39,7 @@ class FaceRecSys:
 
         Btn1 = Button(BgImg, text='STUDENT DETAILS', cursor='hand2',
                       font=("Calibri", 12, 'bold',),
-                      bg='darkblue', fg='white')
+                      bg='black', fg='white')
         Btn1.place(x=200, y=320, width=129, height=30)
 
         # Face Detect Button (img5)
@@ -54,7 +52,7 @@ class FaceRecSys:
 
         Btn2 = Button(BgImg, text='DETECT FACE', cursor='hand2',
                       font=("Calibri", 12, 'bold',),
-                      bg='darkblue', fg='white')
+                      bg='black', fg='white')
         Btn2.place(x=450, y=320, width=129, height=30)
 
 
