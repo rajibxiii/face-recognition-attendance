f6d89876b00bda129c9936cfafa50ec285ae2c5f
Sabit-Bin-Hamid <74405314+Sabit-Bin-Hamid@users.noreply.github.com>
2021-08-08 22:26:00 +0600

    work in student.py
    add student information data ,in student.py make radio button, set labeling and combo box for data entry

--- student.py
+++ student.py
@@ -1,5 +1,5 @@
 from tkinter import*
-# from tkinter import ttk
+from tkinter import ttk
 from PIL import Image, ImageTk
 
 
@@ -34,6 +34,165 @@ class Student:
         # left label Frame
         left_frame= LabelFrame (main_frame, bd=2, bg="white", relief=RIDGE, text="Student Detals", font=("Calibri", 12, "bold"))
         left_frame.place(x=15, y=10, width=730, height=560)
+
+
+        img_left = Image.open(r"images\colorBg.png")
+        img_left = img_left.resize((720, 130), Image.ANTIALIAS)
+        self.PhoImgLeft = ImageTk.PhotoImage(img_left)
+
+        left_frame_lable =  Label(left_frame,image=self.PhoImgLeft)
+        left_frame_lable.place(x=5,y=0,width=720,height=80)
+
+        # current course information (student details left side)
+        current_course_frame = LabelFrame( left_frame, bd=2, bg="white", relief=RIDGE, text="Cureent Course Information",
+                                font=("Calibri", 12, "bold"))
+        current_course_frame .place(x=5, y=80, width=720, height=150)
+
+        # Department labeling and combobox making
+        depart_label = Label(current_course_frame,text="Department",font=('times new roman',13,'bold'),bg='white')
+        depart_label.grid(row=0,column=0,padx=10)
+
+        depart_combo_box=ttk.Combobox(current_course_frame,font=('times new roman',13,'bold'),state='readonly',width=20)
+        depart_combo_box['value']=("select department",'Electrical Engineering ','Computer Engineering ','Architecture','Civil','BBA','Economics','Marketing','Law')
+        depart_combo_box.current(0)
+        depart_combo_box.grid(row=0,column=1,padx=2,pady=10, sticky=W)
+
+        # Course labeling and combobox making
+        course_label = Label(current_course_frame, text="Course", font=('times new roman', 13, 'bold'), bg='white')
+        course_label.grid(row=0, column=2, padx=10,sticky=W)
+
+        course_combo_box = ttk.Combobox(current_course_frame, font=('times new roman', 13, 'bold'), state='readonly',
+                                        width=20)
+        course_combo_box['value'] = (
+        "select course", 'CSE','EEE','SE' )
+        course_combo_box.current(0)
+        course_combo_box.grid(row=0, column=3, padx=2, pady=10, sticky=W)
+
+        # Year labeling and combobox making
+
+        year_label = Label(current_course_frame, text="Year", font=('times new roman', 13, 'bold'), bg='white')
+        year_label.grid(row=1, column=0, padx=10, sticky=W)
+
+        year_combo_box = ttk.Combobox(current_course_frame, font=('times new roman', 13, 'bold'), state='readonly',
+                                        width=20)
+        year_combo_box ['value'] = (
+            "select year", 2020,2021,2022,2023,2024)
+        year_combo_box.current(0)
+        year_combo_box.grid(row=1, column=1, padx=2, pady=10, sticky=W)
+
+        # semester labeling and combobox making
+
+        semester_label = Label(current_course_frame, text="Semester", font=('times new roman', 13, 'bold'), bg='white')
+        semester_label.grid(row=1, column=2, padx=10, sticky=W)
+
+        semester_combo_box = ttk.Combobox(current_course_frame, font=('times new roman', 13, 'bold'), state='readonly',
+                                        width=20)
+        semester_combo_box['value'] = (
+            "select semester", 'summer','fall','spring' )
+        semester_combo_box.current(0)
+        semester_combo_box.grid(row=1, column=3, padx=2, pady=10, sticky=W)
+
+
+        # University student information
+        student_information_frame = LabelFrame(left_frame, bd=2, bg="white", relief=RIDGE, text="Student Information",
+                                          font=("Calibri", 12, "bold"))
+        student_information_frame.place(x=5, y=235, width=720, height=300)
+
+        #student Id label and entry field
+        studentId_label = Label(student_information_frame, text="Student ID", font=('times new roman', 13, 'bold'), bg='white')
+        studentId_label.grid(row=0, column=0, padx=10, sticky=W)
+
+        studentId_entry_field = ttk.Entry(student_information_frame,width=20,font=('times new roman', 13, 'bold'))
+        studentId_entry_field.grid(row=0,column=1,padx=10,pady=5, sticky=W)
+
+        # student name label and entry field
+        student_name_label = Label(student_information_frame, text="Student Name", font=('times new roman', 13, 'bold'),
+                                bg='white')
+        student_name_label.grid(row=0, column=2, padx=10,pady=5, sticky=W)
+
+        student_name_entry_field = ttk.Entry(student_information_frame, width=20, font=('times new roman', 13, 'bold'))
+        student_name_entry_field.grid(row=0, column=3, padx=10,pady=5, sticky=W)
+
+        # student section label and entry field
+        student_section_label = Label(student_information_frame, text="Section", font=('times new roman', 13, 'bold'),
+                                   bg='white')
+        student_section_label.grid(row=1, column=0, padx=10, pady=5, sticky=W)
+
+        student_section_entry_field = ttk.Entry(student_information_frame, width=20, font=('times new roman', 13, 'bold'))
+        student_section_entry_field.grid(row=1, column=1, padx=10, pady=5, sticky=W)
+
+        # student Gender label and entry field
+        student_gender_label = Label(student_information_frame, text="Gender", font=('times new roman', 13, 'bold'),
+                                   bg='white')
+        student_gender_label.grid(row=1, column=2, padx=10, pady=5, sticky=W)
+
+        student_gender_entry_field = ttk.Entry(student_information_frame, width=20, font=('times new roman', 13, 'bold'))
+        student_gender_entry_field.grid(row=1, column=3, padx=10, pady=5, sticky=W)
+
+        # student Date of birth label and entry field
+        student_birthdate_label = Label(student_information_frame, text="Birth Date", font=('times new roman', 13, 'bold'),
+                                      bg='white')
+        student_birthdate_label .grid(row=2, column=0, padx=10, pady=5, sticky=W)
+
+        student_section_entry_field = ttk.Entry(student_information_frame, width=20,
+                                                font=('times new roman', 13, 'bold'))
+        student_section_entry_field.grid(row=2, column=1, padx=10, pady=5, sticky=W)
+
+        # student Gmail label and entry field
+        student_gmail_label = Label(student_information_frame, text="Email",
+                                        font=('times new roman', 13, 'bold'),
+                                        bg='white')
+        student_gmail_label.grid(row=2, column=2, padx=10, pady=5, sticky=W)
+
+        student_gmail_entry_field = ttk.Entry(student_information_frame, width=20,
+                                                font=('times new roman', 13, 'bold'))
+        student_gmail_entry_field.grid(row=2, column=3, padx=10, pady=5, sticky=W)
+
+        # student Phone label and entry field
+        student_phoneno_label = Label(student_information_frame, text="Phone No",
+                                    font=('times new roman', 13, 'bold'),
+                                    bg='white')
+        student_phoneno_label.grid(row=3, column=0, padx=10, pady=5, sticky=W)
+
+        student_phoneno_entry_field = ttk.Entry(student_information_frame, width=20,
+                                              font=('times new roman', 13, 'bold'))
+        student_phoneno_entry_field.grid(row=3, column=1, padx=10, pady=5, sticky=W)
+
+        # student address label and entry field
+        student_address_label = Label(student_information_frame, text="Address",
+                                      font=('times new roman', 13, 'bold'),
+                                      bg='white')
+        student_address_label.grid(row=3, column=2, padx=10, pady=5, sticky=W)
+
+        student_address_entry_field = ttk.Entry(student_information_frame, width=20,
+                                                font=('times new roman', 13, 'bold'))
+        student_address_entry_field.grid(row=3, column=3, padx=10, pady=5, sticky=W)
+
+        # student instructor label and entry field
+        student_instructor_label = Label(student_information_frame, text="instructor Name",
+                                      font=('times new roman', 13, 'bold'),
+                                      bg='white')
+        student_instructor_label.grid(row=4, column=0, padx=10, pady=5, sticky=W)
+
+        student_instructor_entry_field = ttk.Entry(student_information_frame, width=20,
+                                                font=('times new roman', 13, 'bold'))
+        student_instructor_entry_field.grid(row=4, column=1, padx=10, pady=5, sticky=W)
+
+
+
+         # radio buttons
+        radio_button1=ttk.Radiobutton(student_information_frame,text='take photo sample',value='Yes')
+        radio_button1.grid(row=6,column=0)
+
+        radio_button2 = ttk.Radiobutton(student_information_frame, text='no photo sample', value='No')
+        radio_button2.grid(row=6, column=1)
+
+
+        #button frame for student details left side part
+        button_frame = Frame(student_information_frame,bd=2,relief=RIDGE,bg='white')
+        button_frame.place(x=0,y=200,width=715,height=70)
+
+
 
         # right label Frame
         right_frame= LabelFrame (main_frame, bd=2, bg="white", relief=RIDGE, text="Student Detals", font=("Calibri", 12, "bold"))
