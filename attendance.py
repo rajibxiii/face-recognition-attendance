f9e6ee29829b8307a977ea3a205121bde56eceb4
Rajib <48186532+rajibxiii@users.noreply.github.com>
2021-08-16 16:53:23 +0600

    Added all the frames in Attendance

--- attendance.py
+++ attendance.py
@@ -35,7 +35,37 @@ class Attendance:
         )
         titleLabel.place(x=0, y=120, width=1530, height=50)
 
+        main_frame = Frame(BgImg, bd=2, bg="white")
+        main_frame.place(x=10, y=182, width=1505, height=592)
 
+        # left label Frame
+        left_frame = LabelFrame(
+            main_frame,
+            bd=3,
+            bg="white",
+            relief=RIDGE,
+            text="ATTENDANCE",
+            font=("Calibri", 12),
+        )
+        left_frame.place(x=15, y=10, width=740, height=565)
+
+        img_left = Image.open(r"images\colorBg.png")
+        img_left = img_left.resize((720, 130), Image.ANTIALIAS)
+        self.PhoImgLeft = ImageTk.PhotoImage(img_left)
+
+        left_frame_lable = Label(left_frame, image=self.PhoImgLeft)
+        left_frame_lable.place(x=5, y=0, width=720, height=80)
+
+        # right label Frame
+        right_frame = LabelFrame(
+            main_frame,
+            bd=3,
+            bg="white",
+            relief=RIDGE,
+            text="ATTENDANCE",
+            font=("Calibri", 12),
+        )
+        right_frame.place(x=760, y=10, width=730, height=565)
 
 
 if __name__ == "__main__":
