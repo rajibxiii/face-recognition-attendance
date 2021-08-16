a8572d34945cea4262a4825b8be949250225c546
Rajib <48186532+rajibxiii@users.noreply.github.com>
2021-08-16 18:11:25 +0600

    Formatted Face Recognition File

--- performfacerecognition.py
+++ performfacerecognition.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
 from tkinter import messagebox
 import mysql.connector
 import cv2
@@ -14,14 +14,14 @@ class Face_Recognition:
     def __init__(self, root):
         self.root = root
         self.root.geometry("1530x790+0+0")
-        self.root.title('Face Recognition System')
+        self.root.title("Face Recognition System")
 
         titleLabel = Label(
             self.root,
-            text='Face Recognition',
-            font=("Calibri Light",30),
+            text="Face Recognition",
+            font=("Calibri Light", 30),
             # bg="#E8F0F2",
-            bg = '#3F0D12',
+            bg="#3F0D12",
             fg="white",
         )
         titleLabel.place(x=0, y=0, width=1530, height=60)
@@ -32,7 +32,6 @@ class Face_Recognition:
 
         left_frame_lable = Label(self.root, image=self.PhoImgTop)
         left_frame_lable.place(x=0, y=55, width=1530, height=700)
-
 
         # FACE RECOGNITION Button
         Btn1 = Button(
@@ -46,9 +45,8 @@ class Face_Recognition:
         )
         Btn1.place(x=570, y=710, width=400, height=40)
 
-
     # Taking attendance
-    def attendance_marking (self, name, id, course, department):
+    def attendance_marking(self, name, id, course, department):
         with open("attendance.csv", "r+", newline="\n") as att:
             attDataList = att.readlines()
             nameList = []
@@ -57,13 +55,17 @@ class Face_Recognition:
                 entry == line.split((","))
                 nameList.append(entry[0])
 
-            if((id not in nameList) and (name not in nameList)) and (course not in nameList) and (department not in nameList):
+            if (
+                ((id not in nameList) and (name not in nameList))
+                and (course not in nameList)
+                and (department not in nameList)
+            ):
                 now = datetime.datetime.now()
                 d1 = now.strftime("%d.%m.%Y")
                 dtString = now.strftime("%H:%M:%S")
-                att.writelines(f"\n{id},{name},{course},{department},{dtString},{d1},Present")
-
-
+                att.writelines(
+                    f"\n{id},{name},{course},{department},{dtString},{d1},Present"
+                )
 
     # Function for Face Recognition
     def face_recog(self):
@@ -79,15 +81,18 @@ class Face_Recognition:
             for (x, y, width, height) in features:
                 # create a rectangle
                 cv2.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 3)
-                id, predict = clf.predict(gray_img[y:y + height, x:x + width])
+                id, predict = clf.predict(gray_img[y : y + height, x : x + width])
                 confidence = int((100 * (1 - predict / 300)))
-                connection = mysql.connector.connect(host='localhost', username='cse299',
-                                                     password="p2JaZ6@k",
-                                                     database="face_recognition")
+                connection = mysql.connector.connect(
+                    host="localhost",
+                    username="cse299",
+                    password="p2JaZ6@k",
+                    database="face_recognition",
+                )
 
                 # cursor()=> this is an inbuilt function and used here to execute mysql query
                 cursor = connection.cursor()
-                
+
                 query_name = "select Name from student where Student_ID=" + str(id)
                 cursor.execute(query_name)
                 name = cursor.fetchone()
@@ -103,43 +108,80 @@ class Face_Recognition:
                 course = cursor.fetchone()
                 course = "+".join(course)
 
-                query_department = "select Department from student where Student_ID=" + str(id)
+                query_department = (
+                    "select Department from student where Student_ID=" + str(id)
+                )
                 cursor.execute(query_department)
                 department = cursor.fetchone()
                 department = "+".join(department)
-             
 
                 # confidence is work how long we know the face and also give a value
                 if confidence > 77:
-                    cv2.putText(img, f'Name: {name}', (x, y - 75),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
+                    cv2.putText(
+                        img,
+                        f"Name: {name}",
+                        (x, y - 75),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        0.8,
+                        (255, 255, 255),
+                        3,
+                    )
 
-                    cv2.putText(img, f'ID: {id}', (x, y - 55),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
+                    cv2.putText(
+                        img,
+                        f"ID: {id}",
+                        (x, y - 55),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        0.8,
+                        (255, 255, 255),
+                        3,
+                    )
 
-                    cv2.putText(img, f'Course: {course}', (x, y - 30),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
+                    cv2.putText(
+                        img,
+                        f"Course: {course}",
+                        (x, y - 30),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        0.8,
+                        (255, 255, 255),
+                        3,
+                    )
 
-                    cv2.putText(img, f'Department: {department}', (x, y - 5),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
+                    cv2.putText(
+                        img,
+                        f"Department: {department}",
+                        (x, y - 5),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        0.8,
+                        (255, 255, 255),
+                        3,
+                    )
 
                     self.attendance_marking(id, name, course, department)
 
                 else:
                     cv2.rectangle(img, (x, y), (x + width, y + height), (0, 0, 255), 3)
-                    cv2.putText(img, "Unknown person unable to detect", (x, y - 5),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
+                    cv2.putText(
+                        img,
+                        "Unknown person unable to detect",
+                        (x, y - 5),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        0.8,
+                        (255, 255, 255),
+                        3,
+                    )
 
                 coord = [x, y, width, height]
 
             return coord
 
-
         def Recognize(img, clf, faceCascade):
-            coord = drawBoundary(img, faceCascade, 1.1, 10, (255, 25, 2555), "Face", clf)
+            coord = drawBoundary(
+                img, faceCascade, 1.1, 10, (255, 25, 2555), "Face", clf
+            )
             return img
 
-        faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
         clf = cv2.face.LBPHFaceRecognizer_create()
         clf.read("classifier.xml")
 
@@ -157,7 +199,6 @@ class Face_Recognition:
 
         cv2.destroyAllWindows()
 
-            
 
 if __name__ == "__main__":
     root = Tk()  # root is needed to call by toolkit (tk)
