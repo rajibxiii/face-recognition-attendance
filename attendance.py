6ee5f3d65180ccc52e55aba6e1f21b2e848d7597
Rajib <48186532+rajibxiii@users.noreply.github.com>
2021-08-16 17:22:03 +0600

    Added all the detailing labels and entry fields

--- attendance.py
+++ attendance.py
@@ -67,6 +67,124 @@ class Attendance:
         )
         right_frame.place(x=760, y=10, width=730, height=565)
 
+        insideLefFrame = Frame(left_frame, bd=2, relief=RIDGE, bg="white")
+        insideLefFrame.place(x=5, y=135, width=720, height=300)
+
+        # Entry - labelland
+
+        # Attendance Id label and entry field
+        attendanceId_label = Label(
+            insideLefFrame,
+            text="Attendance ID:",
+            font=("Calibri", 13),
+            bg="white",
+        )
+        attendanceId_label.grid(row=0, column=0, padx=10, pady=5, sticky=W)
+
+        attendanceId_entry_field = ttk.Entry(
+            insideLefFrame,
+            width=20,
+            font=("Calibri", 13),
+        )
+        attendanceId_entry_field.grid(row=0, column=1, padx=10, pady=5, sticky=W)
+
+
+        # Name label and entry field
+        name_label = Label(
+            insideLefFrame,
+            text="Name:",
+            font=("Calibri", 13),
+            bg="white",
+        )
+        name_label.grid(row=0, column=2, padx=4, pady=8)
+
+        name_entry_field = ttk.Entry(
+            insideLefFrame,
+            width=20,
+            font=("Calibri", 13),
+        )
+        name_entry_field.grid(row=0, column=3, pady=8)
+
+
+        # Student ID label and entry field
+        id_label = Label(
+            insideLefFrame,
+            text="ID:",
+            font=("Calibri", 13),
+            bg="white",
+        )
+        id_label.grid(row=1, column=0)
+
+        id_entry_field = ttk.Entry(
+            insideLefFrame,
+            width=20,
+            font=("Calibri", 13),
+        )
+        id_entry_field.grid(row=1, column=1, pady=8)
+
+
+        # Department label and entry field
+        department_label = Label(
+            insideLefFrame,
+            text="Department:",
+            font=("Calibri", 13),
+            bg="white",
+        )
+        department_label.grid(row=1, column=2)
+
+        department_entry_field = ttk.Entry(
+            insideLefFrame,
+            width=20,
+            font=("Calibri", 13),
+        )
+        department_entry_field.grid(row=1, column=3, pady=8)
+
+
+        # Date label and entry field
+        date_label = Label(
+            insideLefFrame,
+            text="Date:",
+            font=("Calibri", 13),
+            bg="white",
+        )
+        date_label.grid(row=2, column=0)
+
+        date_entry_field = ttk.Entry(
+            insideLefFrame,
+            width=20,
+            font=("Calibri", 13),
+        )
+        date_entry_field.grid(row=2, column=1, pady=8)
+
+
+        # Time label and entry field
+        time_label = Label(
+            insideLefFrame,
+            text="Time:",
+            font=("Calibri", 13),
+            bg="white",
+        )
+        time_label.grid(row=2, column=2)
+
+        time_entry_field = ttk.Entry(
+            insideLefFrame,
+            width=20,
+            font=("Calibri", 13),
+        )
+        time_entry_field.grid(row=2, column=3, pady=8)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     root = Tk()  # root is needed to call by toolkit (tk)
